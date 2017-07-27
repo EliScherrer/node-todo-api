ee94ee4a913d581d0cd2eb76fd2aaf7f7a965cf8
EliScherrer <escherre@purdue.edu>
2017-07-27 16:46:02 -0400

    added DELETE /todos route

--- server/server.js
+++ server/server.js
@@ -51,6 +51,28 @@ app.get('/todos/:id', (req, res) => {
 		}).catch((err) => {
 			res.status(400).send();
 		})
+});
+
+app.delete('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		res.status(404).send();
+		return;
+	}
+
+	Todo.findByIdAndRemove(id)
+		.then((todo) => {
+			if (todo) {
+				return res.status(200).send(todo);
+			}
+			else {
+				return res.status(404).send();
+			}
+		})
+		.catch((err) => {
+			res.status(400).send();
+		})
 
 
 });
