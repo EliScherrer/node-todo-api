1f5f81a57f0dcfce8ca6cff239b6c5cfe0138e0e
EliScherrer <escherre@purdue.edu>
2017-07-26 21:19:42 -0400

    added GET /todo/:id route

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ var bodyParser = require('body-parser');
 var { mongoose } = require('./db/mongoose');
 var { Todo } = require('./models/todo');
 var { User } = require('./models/user');
+const { ObjectID } = require('mongodb');
 
 var app = express();
 
@@ -30,6 +31,28 @@ app.get('/todos', (req, res) => {
 	});
 });
 
+app.get('/todos/:id', (req, res) => {
+	var id = req.params.id;
+
+	if (!ObjectID.isValid(id)) {
+		res.status(404).send();
+		return;
+	}
+
+	Todo.findById(id)
+		.then((todo) => {
+			if(todo) {
+				res.status(200).send({todo});
+				return;
+			}
+			res.status(404).send();
+
+		}).catch((err) => {
+			res.status(400).send();
+		})
+
+
+});
 
 
 app.listen(3000, () => {
