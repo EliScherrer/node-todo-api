12016973d4e86dbddee80b639fe744548207df08
EliScherrer <escherre@purdue.edu>
2017-08-12 13:59:08 -0400

    added DELETE /users/me/token route

--- server/models/user.js
+++ server/models/user.js
@@ -54,6 +54,19 @@ UserSchema.methods.generateAuthToken = function () {
   });
 };
 
+UserSchema.methods.removeToken = function (token) {
+	var user = this;
+
+	return user.update({
+		$pull: {
+			tokens: {
+				token: token
+			}
+		}
+	});
+
+};
+
 UserSchema.statics.findByToken = function (token) {
 	var User = this;
 	var decoded;
@@ -74,7 +87,7 @@ UserSchema.statics.findByToken = function (token) {
 	});
 };
 
-UserSchema.statics .findByCredentials = function (email, password) {
+UserSchema.statics.findByCredentials = function (email, password) {
 	var User = this;
 
 	return User.findOne({email}).then((user) => {
